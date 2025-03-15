902e88436542890742d0f7ea83916de274ce6202
Vaibhav Sharma <vs8595257872@gmail.com>
2025-03-15 19:06:35 +0530

    Array

---
+++ array.java
@@ -0,0 +1,34 @@
+package DSA;
+
+public class array {
+    public static void main(String[] args) {
+        // for(int i = 0;i<4;i++){
+        //     System.out.println("vaibhav");
+        // }
+        //loop basic (forloop)
+        //   for(int i=1; i<=10;i++){
+        //     if(i%2 ==0 ){
+        //         System.out.println("prime number");
+        //     }
+        //     else{
+        //     System.out.println(i);
+        //     }
+        // }
+        // loop with if else
+        // for(int i=1; i<=20; i++){
+        //     System.out.println(i*19);
+        // }
+        // Table Printing 
+        // for(int i=1; i<=11; i=i+3){
+        //     System.out.println(i);
+        // }
+        //ap printing 
+        // for(int i = 1 ; i<=60; i++){
+        //     if(60%i==0){
+        //     System.out.println(i);
+        //     }
+        // }
+        // 
+    }
+
+}
