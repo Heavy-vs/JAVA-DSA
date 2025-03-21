249e425f7bb4b4d3a418956899e3c9245a10519e
Vaibhav Sharma <vs8595257872@gmail.com>
2025-03-21 19:47:53 +0530

    pattern and loop

--- basicDSA.java
+++ basicDSA.java
@@ -139,13 +139,47 @@ public class basicDSA {
         // *** 
         // ** 
         // * 
-        for(int i=1;i<=4;i++){
-            for(int j=1;j<=5-i;j++)
-            {
+        // for(int i=1;i<=4;i++){
+        //     for(int j=1;j<=5-i;j++)
+        //     {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // 1
+        // 01
+        // 101
+        // 0101
+            //  for(int i =1;i<=4;i++){
+            //     for(int j = 1;j<=i ;j++){
+            //        if (j%2==0 ){z`
+            //         System.out.print("0");                     
+            //        }
+            //        else{
+            //         System.out.print("1");
+            //        }
+            //     }
+            // System.out.println(); 
+            // }
+             
+        //   *     * 
+        //    *   * 
+        //     * *
+        //      *   
+        //     * *
+        //    *   *
+        //   *     *
+        for(int i = 1; i<=4; i++ ){
+        for(float j =1; j<= i-4;j++){
+            // if (j/2==  ){
+                
                 System.out.print("*");
-            }
-            System.out.println();
+                
+            
         }
+        System.out.println();
+    }
         }
 
     }
